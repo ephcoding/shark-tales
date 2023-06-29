8360b9128c686fb136b51b0c7d1fc95f2783defa
Ephraim <ephraim@modevx.com>
2023-06-29 14:01:48 -0500

    FILES: add shark photo image source links

---
+++ src/constants/shark-info.js
@@ -0,0 +1,395 @@
+export const SHARK_INFO = {
+  oceanicWhiteTip: {
+    id: "001",
+    name: "Oceanic Whitetip Shark",
+    imageSrc: "/oceanic-whitetip-shark.jpg",
+    maxLength: {
+      feet: 13,
+      meters: 4,
+    },
+    maxWeight: {
+      pounds: 370,
+      kilograms: 168,
+    },
+    diet: ["squid", "fish", "seabirds", "sea turtles", "carrion"],
+    swimSpeed: {
+      mph: 5,
+      kph: 8,
+    },
+    location: ["warm waters", "open ocean", "rocky reefs", "coral reefs"],
+  },
+  bronzeWhaler: {
+    id: "002",
+    name: "Bronze Whaler Shark",
+    imageSrc: "/bronze-whaler-shark.jpg",
+    maxLength: {
+      feet: 1,
+      meters: 3.4,
+    },
+    maxWeight: {
+      pounds: 672,
+      kilograms: 305,
+    },
+    diet: ["fish", "octopuses", "smaller sharks", "rays"],
+    swimSpeed: {
+      mph: null,
+      kph: null,
+    },
+    location: ["temperate waters worldwide"],
+  },
+  hammerhead: {
+    id: "003",
+    name: "Hammerhead Shark",
+    imageSrc: "/hammerhead-shark.jpg",
+    maxLength: {
+      feet: 13,
+      meters: 4,
+    },
+    maxWeight: {
+      pounds: 500,
+      kilograms: 227,
+    },
+    diet: ["stingrays", "bony fish", "crabs", "squid", "lobster"],
+    swimSpeed: {
+      mph: 25,
+      kph: 40,
+    },
+    location: ["all the oceans"],
+  },
+  spinner: {
+    id: "004",
+    name: "Spinner Shark",
+    imageSrc: "/spinner-shark.jpg",
+    maxLength: {
+      feet: 9,
+      meters: 3,
+    },
+    maxWeight: {
+      pounds: 200,
+      kilograms: 91,
+    },
+    diet: ["fish", "stingrays", "octopuses", "squid"],
+    swimSpeed: {
+      mph: 46,
+      kph: 74,
+    },
+    location: [
+      "shallow coastal waters",
+      "Atlantic ocean",
+      "Pacific ocean",
+      "Indian ocean",
+      "Mediterranean Sea",
+    ],
+  },
+  wobbegong: {
+    id: "005",
+    name: "Wobbegong",
+    imageSrc: "/wobbegong-shark.jpg",
+    maxLength: {
+      feet: 10,
+      meters: 3,
+    },
+    maxWeight: {
+      pounds: 154,
+      kilograms: 70,
+    },
+    diet: ["fish", "invertebrates"],
+    swimSpeed: {
+      mph: null,
+      kph: null,
+    },
+    location: ["coral reefs", "Asia", "New Guinea", "Australia"],
+  },
+  sandTiger: {
+    id: "006",
+    name: "Sand Tiger Shark",
+    imageSrc: "/sand-tiger-shark.jpg",
+    maxLength: {
+      feet: 10,
+      meters: 3,
+    },
+    maxWeight: {
+      pounds: 350,
+      kilograms: 159,
+    },
+    diet: ["herring", "snappers", "eels", "mackerals", "other sharks"],
+    swimSpeed: {
+      mph: 12,
+      kph: 19,
+    },
+    location: ["warm coastal waters"],
+  },
+  blackTip: {
+    id: "006",
+    name: "Blacktip Reef Shark",
+    imageSrc: "/black-tip-reef-shark.jpg",
+    maxLength: {
+      feet: 8,
+      meters: 2.4,
+    },
+    maxWeight: {
+      pounds: 220,
+      kilograms: 100,
+    },
+    diet: ["fish", "skates", "stingrays", "squid", "crustaceans"],
+    swimSpeed: {
+      mph: 14,
+      kph: 22.5,
+    },
+    location: ["warm coastal waters"],
+  },
+  bull: {
+    id: "007",
+    name: "Bull Shark",
+    imageSrc: "/bull-shark.jpg",
+    maxLength: {
+      feet: 11.5,
+      meters: 3.5,
+    },
+    maxWeight: {
+      pounds: 500,
+      kilograms: 227,
+    },
+    diet: ["fish", "small sharks", "turtles", "birds", "dolphins"],
+    swimSpeed: {
+      mph: 25,
+      kph: 40,
+    },
+    location: [],
+  },
+  tiger: {
+    id: "008",
+    name: "Tiger Shark",
+    imageSrc: "/tiger-shark.jpg",
+    maxLength: {
+      feet: 14,
+      meters: 4.3,
+    },
+    maxWeight: {
+      pounds: 1400,
+      kilograms: 635,
+    },
+    diet: [
+      "stingrays",
+      "sea turtles",
+      "clams",
+      "sea snakes",
+      "seals",
+      "birds",
+      "squid",
+    ],
+    swimSpeed: {
+      mph: 20,
+      kph: 32,
+    },
+    location: ["tropical waters", "subtropical waters"],
+  },
+  greatWhite: {
+    id: "009",
+    name: "Great White Shark",
+    imageSrc: "/great-white-shark.jpg",
+    maxLength: {
+      feet: 20,
+      meters: 6,
+    },
+    maxWeight: {
+      pounds: 5000,
+      kilograms: 2268,
+    },
+    diet: ["fish", "seals", "seal lions", "small whales", "sea turtles"],
+    swimSpeed: {
+      mph: 15,
+      kph: 24,
+    },
+    location: ["cool coastal waters"],
+  },
+  whale: {
+    id: "010",
+    name: "Whale Shark",
+    imageSrc: "/whale-shark.jpg",
+    maxLength: {
+      feet: 40,
+      meters: 12,
+    },
+    maxWeight: {
+      pounds: 41200,
+      kilograms: 18700,
+    },
+    diet: ["plankton", "small fish", "squid"],
+    swimSpeed: {
+      mph: 3,
+      kph: 4.8,
+    },
+    location: ["temperate waters worldwide"],
+  },
+  megamouth: {
+    id: "011",
+    name: "Megamouth Shark",
+    imageSrc: "mega-mouth-shark.jpg",
+    maxLength: {
+      feet: 18,
+      meters: 5.5,
+    },
+    maxWeight: {
+      pounds: 2679,
+      kilograms: 1215,
+    },
+    diet: ["shrimp", "plankton", "small fish", "jellyfish"],
+    swimSpeed: {
+      mph: null,
+      kph: null,
+    },
+    location: ["small ocean patches worldwide"],
+  },
+  blind: {
+    id: "012",
+    name: "Blind Shark",
+    imageSrc: "/blind-shark.jpg",
+    maxLength: {
+      feet: 3.9,
+      meters: 1.2,
+    },
+    maxWeight: {
+      pounds: 45,
+      kilograms: 20.4,
+    },
+    diet: ["sea anemones", "squid", "crustaceans", "small fish"],
+    swimSpeed: {
+      mph: null,
+      kph: null,
+    },
+    location: ["east Australian coast", "rocky areas", "seagrass beds"],
+  },
+  broadnoseSevengill: {
+    id: "013",
+    name: "Broadnose Sevengill Shark",
+    imageSrc: "/broadnose-sevengill-shark.jpg",
+    maxLength: {
+      feet: 9.8,
+      meters: 3,
+    },
+    maxWeight: {
+      pounds: 236,
+      kilograms: 107,
+    },
+    diet: [
+      "other sharks",
+      "bat rays",
+      "harbor seals",
+      "dolphins",
+      "crabs",
+      "dead animal remains",
+    ],
+    swimSpeed: {
+      mph: null,
+      kph: null,
+    },
+    location: ["coastal waters", "waters no deeper than 164' (50 m) worldwide"],
+  },
+  angel: {
+    id: "014",
+    name: "Angel Shark",
+    imageSrc: "/angel-shark.jpg",
+    maxLength: {
+      feet: 4.9,
+      meters: 1.5,
+    },
+    maxWeight: {
+      pounds: 77,
+      kilograms: 34,
+    },
+    diet: ["fish", "crustaceans", "clams", "mussels"],
+    swimSpeed: {
+      mph: 2.5,
+      kph: 4,
+    },
+    location: ["shallow coastal waters worldwide"],
+  },
+  californiaHorn: {
+    id: "015",
+    name: "California Horn Shark",
+    imageSrc: "/california-horn-shark.jpg",
+    maxLength: {
+      feet: 4,
+      meters: 1.2,
+    },
+    maxWeight: {
+      pounds: 60,
+      kilograms: 27,
+    },
+    diet: ["snails", "crabs", "sea urchins", "small fish"],
+    swimSpeed: {
+      mph: 2.3,
+      kph: 3.7,
+    },
+    location: ["coastal waters worldwide"],
+  },
+  velvetBellyLantern: {
+    id: "016",
+    name: "Velvet Belly Lantern Shark",
+    imageSrc: "/velvet-belly-lantern-shark.jpg",
+    maxLength: {
+      feet: 1.5,
+      meters: 0.5,
+    },
+    maxWeight: {
+      pounds: 2,
+      kilograms: 0.9,
+    },
+    diet: ["shellfish", "small fish", "squid"],
+    swimSpeed: {
+      mph: null,
+      kph: null,
+    },
+    location: [
+      "east Atlantic ocean",
+      "western Mediterranean sea",
+      "teh Azores",
+      "Canary Islands, Cape Verde",
+    ],
+  },
+  crocodile: {
+    id: "017",
+    name: "Crocodile Shark",
+    imageSrc: "crocodile-shark.jpg",
+    maxLength: {
+      feet: 3.9,
+      meters: 1.2,
+    },
+    maxWeight: {
+      pounds: null,
+      kilograms: null,
+    },
+    diet: ["fish", "squid", "shrimp"],
+    swimSpeed: {
+      mph: null,
+      kph: null,
+    },
+    location: ["deep tropical waters worldwide"],
+  },
+  lemon: {
+    id: "018",
+    name: "Lemon Shark",
+    imageSrc: "lemon-shark.jpg",
+    maxLength: {
+      feet: 10,
+      meters: 3,
+    },
+    maxWeight: {
+      pounds: 420,
+      kilograms: 190.5,
+    },
+    diet: ["fish"],
+    swimSpeed: {
+      mph: 2,
+      kph: 3.2,
+    },
+    location: [
+      "warm shallow waters",
+      "Africa",
+      "Australia",
+      "North America",
+      "South America",
+    ],
+  },
+};
