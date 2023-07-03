17d014dd15aca86d300845cdf4112f92799810b5
Ephraim <ephraim@modevx.com>
2023-07-03 08:04:33 -0500

    delete unused imports

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,6 +1,3 @@
-import { Navbar } from "react-daisyui";
-import Link from "next/link";
-
 export const PageLayout = ({ children }) => {
   return (
     <main className={`min-h-screen bg-black bg-opacity-50`}>{children}</main>
