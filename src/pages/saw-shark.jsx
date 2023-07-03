87be05b128c83fc7baf62e52ee7635e14cf2f5b9
Ephraim <ephraim@modevx.com>
2023-07-03 08:06:09 -0500

    add Saw Shark page

---
+++ src/pages/saw-shark.jsx
@@ -0,0 +1,12 @@
+import { HomeLinkButton } from "@/components/HomeLinkButton";
+import { PageWithBackgroundImage } from "@/components/PageWithBackgroundImage";
+
+const SawSharkPage = () => {
+  return (
+    <PageWithBackgroundImage bgImgUrl="/saw-shark.jpg">
+      <HomeLinkButton />
+    </PageWithBackgroundImage>
+  );
+};
+
+export default SawSharkPage;
