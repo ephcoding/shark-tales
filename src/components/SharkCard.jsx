567269c5ad389238d3bd887165f5c09ce3d71cf3
Ephraim <ephraim@modevx.com>
2023-07-03 08:04:48 -0500

    build SharkCard component

---
+++ src/components/SharkCard.jsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import { Button, Card } from "react-daisyui";
+
+export const SharkCard = ({ name, imageSrc, url }) => {
+  return (
+    <Card key={name} className="bg-white">
+      <Card.Image src={imageSrc} alt={name} />
+      <Card.Body>
+        <Card.Title tag="h3" className="text-secondary">
+          {name}
+        </Card.Title>
+        <Card.Actions className="justify-end">
+          <Link href={url}>
+            <Button color="primary">Learn more!</Button>
+          </Link>
+        </Card.Actions>
+      </Card.Body>
+    </Card>
+  );
+};
