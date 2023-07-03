a1714bf73f1b262bc34a1624c8b5728b963edbbb
Ephraim <ephraim@modevx.com>
2023-07-03 08:06:03 -0500

    add Megalodon page

---
+++ src/pages/megalodon-shark.jsx
@@ -0,0 +1,12 @@
+import { HomeLinkButton } from "@/components/HomeLinkButton";
+import { PageWithBackgroundImage } from "@/components/PageWithBackgroundImage";
+
+const MegalodonSharkPage = () => {
+  return (
+    <PageWithBackgroundImage bgImgUrl="/megalodon-shark.jpg">
+      <HomeLinkButton />
+    </PageWithBackgroundImage>
+  );
+};
+
+export default MegalodonSharkPage;
