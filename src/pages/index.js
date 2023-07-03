d5b7ed6a4a86f068041c1dd2fad4722fe7328579
Ephraim <ephraim@modevx.com>
2023-07-03 08:05:51 -0500

    sort SharkCards

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,29 @@
-import Link from "next/link";
-import { Button, Card } from "react-daisyui";
-
 import { SHARK_INFO } from "@/constants";
-import { PageWithBackgroundImage } from "@/components/PageWithBackgroundImage";
+import { PageWithBackgroundImage, SharkCard } from "@/components";
+import { sortObjArrByStrProp } from "@/utils";
 
 export default function Home() {
+  const sortedSharks = Object.values(SHARK_INFO).sort((a, b) =>
+    sortObjArrByStrProp(a, b, "name")
+  );
+
   return (
     <PageWithBackgroundImage bgImgUrl="/sharks-from-top.jpg">
-      <div className="grid gap-20 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-10 overflow-auto">
-        {Object.values(SHARK_INFO).map((shark) => {
+      <GridLayout>
+        {sortedSharks.map(({ imageSrc, name, url }) => {
           return (
-            <Card key={shark.name} className="bg-white">
-              <Card.Image src={shark.imageSrc} alt={shark.name} />
-              <Card.Body>
-                <Card.Title tag="h3" className="text-secondary">
-                  {shark.name}
-                </Card.Title>
-                <Card.Actions className="justify-end">
-                  <Link href={shark.url}>
-                    <Button color="primary">Learn more!</Button>
-                  </Link>
-                </Card.Actions>
-              </Card.Body>
-            </Card>
+            <SharkCard key={name} imageSrc={imageSrc} name={name} url={url} />
           );
         })}
-      </div>
+      </GridLayout>
     </PageWithBackgroundImage>
   );
 }
+
+const GridLayout = ({ children }) => {
+  return (
+    <div className="grid gap-20 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-10 overflow-auto">
+      {children}
+    </div>
+  );
+};
