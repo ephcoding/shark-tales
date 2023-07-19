81a863f5f5b9767e7acfa66f0f93046677423b61
Ephraim <ephraim@modevx.com>
2023-07-19 09:14:05 -0500

    rename util slug conversion func

--- src/pages/sharks/[shark].jsx
+++ src/pages/sharks/[shark].jsx
@@ -6,14 +6,14 @@ import { SHARK_INFO } from "@/constants";
 const SharkPage = () => {
   const nextRouter = useRouter();
   let slug = nextRouter.query.shark;
-  const sharkType = getSharkPropNameFromSlug(slug);
+  const sharkInfoProp = convertSlugToSharkInfoProp(slug);
 
-  return <SharkInfoPage shark={SHARK_INFO[`${sharkType}`]} />;
+  return <SharkInfoPage shark={SHARK_INFO[`${sharkInfoProp}`]} />;
 };
 
 export default SharkPage;
 
-const getSharkPropNameFromSlug = (slug) => {
+const convertSlugToSharkInfoProp = (slug) => {
   const splitName = slug.split("-");
   const endIndex = splitName.length - 1;
   const sharkType = splitName.slice(0, endIndex).join("");
